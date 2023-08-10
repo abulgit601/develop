857202b08abfada7f402018bb38f2d3f03127102
abulgit601 <141927277+abulgit601@users.noreply.github.com>
2023-08-10 18:04:34 +0530

    Create test.php
    first commit

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+class test extends MY_Controller{
+	
+	function testdropship(){
+		parent::MY_Controller();
+		$this->load->model ( "ajio/testdropshipmodel" );
+	}
+	
+	function download($ordernumber){
+echo "test";die;
+}
+
+?>
